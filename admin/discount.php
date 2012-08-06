2eb34312abf930f39ddcef8db2eb7ce497a89456
happywang <365576140@qq.com>
2012-08-06 22:30:27 +0800

    20120806-22:30

--- admin/discount.php
+++ admin/discount.php
@@ -36,6 +36,9 @@ if ($_REQUEST['act'] == 'list')//折扣券列表
 		}
 		array_push($coupons_list,$value);
 	}
+	$smarty->assign('ur_here', '折扣券');
+	$action_link = array('href' => 'discount.php?act=add', 'text' => '添加折扣券');
+	$smarty->assign('action_link',  $action_link);
 	$smarty->assign('coupons_list', $coupons_list);
 	$smarty->display('discount_list.htm');
 }
@@ -63,6 +66,9 @@ elseif($_REQUEST['act'] == 'edit')
 	{
 		$row['goods_name'] = '--';
 	}
+	$row['goods_id'] = $row['goods_id'] ? $row['goods_id'] : '';
+	$smarty->assign('coupons_id', $row['coupons_id']);
+	$smarty->assign('goods_id', $row['goods_id']);
 	$smarty->assign('coupons_code', $row['coupons_code']);
 	$smarty->assign('coupons_rate', $row['coupons_rate']);
 	$smarty->assign('coupons_subtracting_amount', $row['coupons_subtracting_amount']);
@@ -71,10 +77,111 @@ elseif($_REQUEST['act'] == 'edit')
 	$smarty->assign('goods_name', $row['goods_name']);
 	$smarty->assign('use_times', $row['use_times']);
 	$smarty->assign('expiration_time', $row['expiration_time']);
+	$smarty->assign('insert_date', $row['insert_date']);
 	$smarty->assign('Remarks', $row['Remarks']);
 	$smarty->assign('cat_list', cat_list(0, $row['cat_id']));
 	$smarty->display('discount_info.htm');
 }
 elseif($_REQUEST['act'] == 'update'){
-// 	$coupons_code = 
+	$coupons_id = isset($_REQUEST['coupons_id']) ? trim($_REQUEST['coupons_id']) : '';
+	$goods_id = isset($_REQUEST['goods_id']) ? trim($_REQUEST['goods_id']) : '';
+	$coupons_code = isset($_REQUEST['coupons_code']) ? trim($_REQUEST['coupons_code']) : '';
+	$coupons_rate = isset($_REQUEST['coupons_rate']) ? trim($_REQUEST['coupons_rate']) : '';
+	$coupons_subtracting_amount = isset($_REQUEST['coupons_subtracting_amount']) ? trim($_REQUEST['coupons_subtracting_amount']) : '';
+	$user_email = isset($_REQUEST['user_email']) ? trim($_REQUEST['user_email']) : '';
+	$cat_id = isset($_REQUEST['cat_id']) ? trim($_REQUEST['cat_id']) : '';
+	$goods_name = isset($_REQUEST['goods_name']) ? trim($_REQUEST['goods_name']) : '';
+	$use_times = isset($_REQUEST['use_times']) ? trim($_REQUEST['use_times']) : '';
+	$expiration_time = isset($_REQUEST['expiration_time']) ? trim($_REQUEST['expiration_time']) : '';
+	$Remarks = isset($_REQUEST['Remarks']) ? trim($_REQUEST['Remarks']) : '';
+	if($goods_id && $cat_id)
+	{
+		$goodSql = "SELECT * FROM " .$ecs->table('goods'). " where goods_id={$goods_id} and `cat_id `={$cat_id} limit 1";
+		$row = $db->getOne($goodSql);
+		if($row)
+		{
+			echo json_encode(array('Ret'=>-1));
+			exit;
+		}
+	}
+	$sql = "UPDATE " . $ecs->table('discount_coupons') . " SET " .
+			"coupons_code = 'coupons_code', " .
+			"coupons_rate = '$coupons_rate', " .
+			"coupons_subtracting_amount = '$coupons_subtracting_amount', " .
+			"user_email = '$user_email', " .
+			"cat_id = '$cat_id', " .
+			"goods_id = '$goods_id', " .
+			"use_times = '$use_times', " .
+			"expiration_time = '$expiration_time', " .
+			"Remarks = '$Remarks', " .
+			"insert_date = "."date('Y-m-d H:i:s')"." WHERE `coupons_id` = '$coupons_id'";
+	$result = $db->query($sql);
+	if($result)
+	{
+		echo json_encode(array('Ret'=>1));
+	}
+	else
+	{
+		echo json_encode(array('Ret'=>-2));
+	}
+	exit;
+}
+elseif($_REQUEST['act'] == 'del'){
+	$sql = "DELETE FROM {$ecs->table('discount_coupons')} WHERE `coupons_id` = {$_POST['coupons_id']} LIMIT 1";
+	$result = $db->query($sql);
+	if($result)
+	{
+		echo json_encode(array('Ret'=>1));
+	}
+	else
+	{
+		echo json_encode(array('Ret'=>-2));
+	}
+	exit;
+}
+elseif($_REQUEST['act'] == 'add'){
+	$smarty->assign('cat_list', cat_list(0, ''));
+	$smarty->display('discount_add.htm');
+}
+elseif($_REQUEST['act'] == 'insert'){
+	$coupons_code = isset($_REQUEST['coupons_code']) ? trim($_REQUEST['coupons_code']) : '';
+	$coupons_rate = isset($_REQUEST['coupons_rate']) ? trim($_REQUEST['coupons_rate']) : '';
+	$coupons_subtracting_amount = isset($_REQUEST['coupons_subtracting_amount']) ? trim($_REQUEST['coupons_subtracting_amount']) : '';
+	$user_email = isset($_REQUEST['user_email']) ? trim($_REQUEST['user_email']) : '';
+	$cat_id = isset($_REQUEST['cat_id']) ? trim($_REQUEST['cat_id']) : '';
+	$goods_name = isset($_REQUEST['goods_name']) ? trim($_REQUEST['goods_name']) : '';
+	$use_times = isset($_REQUEST['use_times']) ? trim($_REQUEST['use_times']) : 0;
+	$expiration_time = isset($_REQUEST['expiration_time']) ? trim($_REQUEST['expiration_time']) : '';
+	$Remarks = isset($_REQUEST['Remarks']) ? trim($_REQUEST['Remarks']) : '';
+	if($goods_name)
+	{
+		$goodSql = "SELECT `goods_id` FROM " .$ecs->table('goods'). " where goods_name={$goods_name} limit 1";
+		$goods_id = $db->getOne($goodSql);
+	}
+	else 
+	{
+		$goods_id = '';
+	}
+	$couponsSql = "SELECT `coupons_id` FROM " .$ecs->table('discount_coupons'). " where coupons_code={$coupons_code} limit 1";
+	$coupons_id = $db->getOne($couponsSql);
+	if($coupons_id)
+	{
+		echo json_encode(array('Ret'=>-1));
+		exit;
+	}
+	$insert_time = date('Y-m-d H:i:s');
+	$sql = "INSERT INTO " . $ecs->table('discount_coupons') . " (coupons_code, coupons_rate, coupons_subtracting_amount, " .
+			"user_email, cat_id, goods_id, use_times, expiration_time, Remarks,insert_date) " .
+			"VALUES ('$coupons_code', '$coupons_rate', '$coupons_subtracting_amount', '$user_email', " .
+			"'$cat_id', '$goods_id', '$use_times', '$expiration_time','$Remarks', " . "'$insert_time'".")";
+	$result = $db->query($sql);
+	if($result)
+	{
+		echo json_encode(array('Ret'=>1));
+	}
+	else
+	{
+		echo json_encode(array('Ret'=>-2));
+	}
+	exit;
 }
